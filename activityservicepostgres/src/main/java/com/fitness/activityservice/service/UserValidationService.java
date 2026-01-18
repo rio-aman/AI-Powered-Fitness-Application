e23102d1a41b0a0979228c4fdcff5257278acea4
rio-aman <amprajapati08893.com>
2026-01-18 13:07:25 +0530

    AI-Powered-Fitness-Application starting for the gemini ai service for the content generate and send to the user

---
+++ activityservicepostgres/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -0,0 +1,28 @@
+package com.fitness.activityservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserValidationService {
+    private final WebClient userServiceWebClient;
+
+    public boolean validateUser(String userId) {
+        log.info("Calling User Validation API for userId: {}", userId);
+        try{
+            return userServiceWebClient.get()
+                    .uri("/api/users/{userId}/validate", userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
