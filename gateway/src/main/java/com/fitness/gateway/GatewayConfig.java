84de3ab4acdef9d11e1de97eb0b63aa2e7647bd0
rio-aman <amprajapati08893.com>
2026-01-20 13:48:29 +0530

    AI-Powered-Fitness-Application converted the gateway into class based GatewayConfig configuration

--- gateway/src/main/java/com/fitness/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/fitness/gateway/GatewayConfig.java
@@ -12,7 +12,7 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("activity-service", r -> r
-                        .path("/api/activities")
+                        .path("/api/activities/**")
                         .uri("lb://ACTIVITY-SERVICE"))
                 .route("user-service", r -> r
                         .path("/api/users/**")
